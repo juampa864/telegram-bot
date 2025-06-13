b25313cfa143e76c5196ab5b1fb7f6037a99c7d8
JP Rodriguez Illescas <38998215+juampa864@users.noreply.github.com>
2025-06-13 11:03:30 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
-// colors :p
+// OPTIONAL
+// Used to make the terminal more user friendly.
 const colors = require('colors');
 
-// Server configuration, cors and express. 
+// OPTIONAL, but highly recommended.
+// Using the .env file to store sensible information like the bot's token.
+// This will load the .env file into the process.env variable.
+// The .env file should be in root directory.
+require('dotenv').config()
+
+// Importing server configuration, CORS and Express. 
+// See config/server.js for more details.
 const app = require('./config/server');
 
-
+// Import bot 
+// See app/bot.js for more details.
+// The server is passed as an argument to the bot, meaning the bot can handle http queries.
 require('./app/bot')(app);
 
 // Server connection 
