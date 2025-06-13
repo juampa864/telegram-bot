edaaef151253c6faac2f3cc2a98b6e063334a936
JP Rodriguez Illescas <38998215+juampa864@users.noreply.github.com>
2025-06-13 11:02:09 -0600

    Update server.js

--- src/config/server.js
+++ src/config/server.js
@@ -1,13 +1,19 @@
-// configuration of the servrer
+// Configuration for the server.
+// Import the required modules.
 const express = require('express');
+// CORS is used to handle requests from different domains or origins in the web app.
 const cors = require('cors');
+
+// Express is a middleware that provides an abstracton layer to handle routing and HTTP requests.
 const app = express();
 
-// this tells the app that the format will be json
+// This tells the app that the format will be JSON for responses.
 app.use(express.json());
-// this allows communication between routes
+// This allows communication between routes, tells the app to accept requests from all origins.
 app.use(cors());
 
+// Sets a port for the server, it can use the 3040 port or the port defined in process.env.PORT.
 app.set('port', process.env.PORT || 3040);
 
+// For a more modular implementation, the server.js file exports the server configuration to be imported in other files.
 module.exports = app;
